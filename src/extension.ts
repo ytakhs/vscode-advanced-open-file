6b0fb75190a872ea69e6013ef005a390d32c7f47
YUJI takahashi <jit.y.31@gmail.com>
2019-01-19 21:12:15 +0900

    pick item

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,18 @@
 'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import {
+  commands,
+  window,
+  workspace,
+  ExtensionContext,
+} from "vscode";
+
+import { sync as globSync } from "glob"
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
@@ -14,11 +21,19 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let disposable = commands.registerCommand('extension.advancedOpenFile', async () => {
         // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        let files = await workspace.findFiles("**", "**/node_modules/**", 10)
+        // let folders = [...workspace.workspaceFolders].map(f => { return f.name })
+        // let filenames: string[] = [...files].map(file => { return file.toString() })
+        let fs = globSync("**", { ignore: "**/node_modules/**" }).map(path => {
+            return path
+        })
+        // const quickPick = window.createQuickPick()
+        // quickPick.items = filenames
+        // const items = [...folders, ...filenames]
+        const picked = await window.showQuickPick(fs)
+        window.showInformationMessage(picked)
     });
 
     context.subscriptions.push(disposable);
