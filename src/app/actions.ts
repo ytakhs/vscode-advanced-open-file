afc2d0dcdc9914706e833571492528251d7102bf
ytakhs <21127627+ytakhs@users.noreply.github.com>
2024-09-21 22:30:09 +0900

    Add actions

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,8 +1,14 @@
+import { Uri } from "vscode";
+import { createFileItems, type FileItem } from "../fileItem";
 import type { Options } from "./options";
 import type { State } from "./state";
 
 export type Actions = {
-  pick: () => void;
+  pick: () => Promise<void>;
+  getUri: () => Uri;
+  setUri: (uri: Uri) => void;
+  joinUri: (...pathSegments: string[]) => void;
+  setItems: (items: ReadonlyArray<FileItem>) => void;
 };
 
 type ActionParams = {
@@ -12,13 +18,36 @@ type ActionParams = {
 
 export const initActions = (state: State, options: Options): Actions => {
   return {
-    pick: initPickAction({ state, options }),
+    pick: initPick({ state, options }),
+    getUri: () => state.currentUri,
+    setUri: initSetUri({ state, options }),
+    joinUri: initJoinUri({ state, options }),
+    setItems: (items: ReadonlyArray<FileItem>) => {
+      state.picker.items = items;
+    },
   };
 };
 
-const initPickAction = ({
-  state: { currentUri, picker },
-  options,
-}: ActionParams) => {
-  return () => {};
+const initPick = ({ state: { picker, currentUri } }: ActionParams) => {
+  return async () => {
+    picker.show();
+    picker.value = currentUri.fsPath;
+    picker.items = await createFileItems(currentUri.fsPath);
+  };
+};
+
+const initSetUri = ({ state }: ActionParams) => {
+  return (uri: Uri) => {
+    state.picker.value = uri.fsPath;
+    state.currentUri = uri;
+  };
+};
+
+const initJoinUri = ({ state }: ActionParams) => {
+  return (...pathSegments: string[]) => {
+    const uri = Uri.joinPath(state.currentUri, ...pathSegments);
+
+    state.picker.value = uri.fsPath;
+    state.currentUri = uri;
+  };
 };
